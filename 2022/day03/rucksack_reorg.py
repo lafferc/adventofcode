60ad678f7112984f4551c4c430d72327d8ae320b
lafferc <lafferc@tcd.ie>
2022-12-03 13:49:26 +0000

    Day 3 puzzle 2

--- 2022/day03/rucksack_reorg.py
+++ 2022/day03/rucksack_reorg.py
@@ -3,6 +3,9 @@ import argparse
 import string
 
 
+g_priorities_order = '_' + string.ascii_lowercase + string.ascii_uppercase
+
+
 class Rucksack:
     def __init__(self):
         self.compartments = [[], []]
@@ -18,6 +21,9 @@ class Rucksack:
 
         return list(c1.intersection(c2))
 
+    def get_all_items(self):
+        return self.compartments[0] + self.compartments[1]
+
 
 def load_items(filename: str) -> List[str]:
     items = []
@@ -30,6 +36,34 @@ def load_items(filename: str) -> List[str]:
 
             items.append(line)
     return items
+
+
+def find_duplicate_items_total_value(rucksacks: List[Rucksack]) -> int:
+    total = 0
+    for r in rucksacks:
+        for item in r.get_duplicate_items():
+            total += g_priorities_order.index(item)
+    return total
+
+
+def find_badge_item(r1: Rucksack, r2: Rucksack, r3: Rucksack) -> str:
+    s1 = set(r1.get_all_items())
+    s2 = set(r2.get_all_items())
+    s3 = set(r3.get_all_items())
+
+    badge = list(s1.intersection(s2, s3))
+
+    assert len(badge) == 1
+
+    return badge[0]
+
+
+def find_badge_items_total_value(rucksacks: List[Rucksack]) -> int:
+    total = 0
+    for i in range(0, len(rucksacks), 3):
+        item = find_badge_item(*rucksacks[i:i+3])
+        total += g_priorities_order.index(item)
+    return total
 
 
 def main():
@@ -47,12 +81,11 @@ def main():
         r.pack(list(items_str))
         rucksacks.append(r)
 
-    total = 0
-    for r in rucksacks:
-        for item in r.get_duplicate_items():
-            total += priorities_order.index(item)
+    # print(find_duplicate_items_total_value(rucksacks))
 
-    print(total)
+    print(find_badge_items_total_value(rucksacks))
+
+
 
 
 if __name__ == "__main__":
