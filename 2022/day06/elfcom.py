44d87b0c55e4dba934ce8b75a74f62f9a90867e7
lafferc <clafferty@druidsoftware.com>
2022-12-06 14:23:35 +0000

    Day 6 puzzle 2

--- 2022/day06/elfcom.py
+++ 2022/day06/elfcom.py
@@ -7,13 +7,13 @@ def load_file(filename: str) -> str:
         return input_file.read()
 
 
-def get_start_of_packet_marker(data: str) -> int:
+def get_start_of_packet_marker(data: str, n_chars) -> int:
     """ return the start-of-packet marker index """
 
     logging.debug("data: %r" % data)
-    for i in range(4, len(data)):
-        logging.debug(f"i:{i}, {data[i-4:i]}")
-        if len(set(data[i-4:i])) == 4:
+    for i in range(n_chars, len(data)):
+        logging.debug(f"i:{i}, {data[i-n_chars:i]}")
+        if len(set(data[i-n_chars:i])) == n_chars:
             return i
     logging.error("End of data")
 
@@ -22,6 +22,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="input filename")
     parser.add_argument("-d", "--debug", help="set debug logging", action="store_true")
+    parser.add_argument("-n", "--num", type=int, default=4, help="number of chars in marker")
     
     args = parser.parse_args()
 
@@ -30,7 +31,7 @@ def main():
 
     data = load_file(args.filename)
 
-    marker = get_start_of_packet_marker(data)
+    marker = get_start_of_packet_marker(data, args.num)
 
     print(marker)
 
