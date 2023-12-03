e7a2d559ab5d50fc604f287c75a2dd4751427d7a
lafferc <lafferc@tcd.ie>
2023-12-03 01:48:59 +0000

    Day1 part 2

--- 2023/day01/calibration.py
+++ 2023/day01/calibration.py
@@ -4,10 +4,11 @@ from typing import List
 
 def load_file(filename: str) -> List:
     with open(filename) as input_file:
-        return input_file.readlines()
+        for line in input_file.readlines():
+            yield line.strip()
 
 
-def extract_values(inlist):
+def extract_digits(inlist):
     for line in inlist:
         first_digit = None
         last_digit = None
@@ -23,6 +24,53 @@ def extract_values(inlist):
         yield int(first_digit + last_digit)
 
 
+g_valid_numbers = {
+        "one": "1",
+        "two": "2",
+        "three": "3",
+        "four": "4",
+        "five": "5",
+        "six": "6",
+        "seven": "7",
+        "eight": "8",
+        "nine": "9"
+        }
+
+def extract_numbers(inlist):
+    for line in inlist:
+        first_digit = None
+        last_digit = None
+        logging.debug(f"{line} - {len(line)}")
+        for i in range(len(line)):
+            logging.debug(f"{i}: {line[i]}")
+
+            if line[i].isdigit():
+                first_digit = line[i]
+                break
+            for num in g_valid_numbers.keys():
+                if line[i:].startswith(num):
+                    first_digit = g_valid_numbers[num]
+                    break
+            if first_digit is not None:
+                break
+
+        for i in range(len(line) - 1, -1, -1):
+            logging.debug(f"{i}: {line[i]}")
+
+            if line[i].isdigit():
+                last_digit = line[i]
+                break
+
+            for num in g_valid_numbers.keys():
+                if line[i:].startswith(num):
+                    last_digit = g_valid_numbers[num]
+                    break
+            if last_digit is not None:
+                break
+
+
+        yield int(first_digit + last_digit)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -34,7 +82,7 @@ def main():
     if args.debug:
         logging.basicConfig(level=logging.DEBUG)
 
-    print(sum(extract_values(load_file(args.filename))))
+    print(sum(extract_numbers(load_file(args.filename))))
 
 
 if __name__ == "__main__":
